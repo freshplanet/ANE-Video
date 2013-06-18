d98a455b0b0a8ce54c55f318b57ea1f5e3823b55
Thibaut Crenn <crenn@freshplanet.com>
2013-06-18 11:24:17 -0400

    clean up on android now deletes video file

--- android/src/com/freshplanet/ane/AirVideo/functions/CleanUpFunction.java
+++ android/src/com/freshplanet/ane/AirVideo/functions/CleanUpFunction.java
@@ -3,12 +3,20 @@ package com.freshplanet.ane.AirVideo.functions;
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
+import com.freshplanet.ane.AirVideo.CreateFileTask;
 import com.freshplanet.ane.AirVideo.Extension;
 
 public class CleanUpFunction implements FREFunction {
 
 	@Override
 	public FREObject call(FREContext arg0, FREObject[] arg1) {
+		
+		String previousFilePath = new CreateFileTask().getPreviousFileName();
+		if (previousFilePath != null)
+		{
+			arg0.getActivity().deleteFile(previousFilePath);
+		}
+
 		Extension.context.cleanUp();
 		return null;
 	}
